cc9a43dbfd9db314486a63fcbda90603fddd2969
Arpitkushwahaa <151647324+Arpitkushwahaa@users.noreply.github.com>
2025-06-15 21:53:34 +0530

    Update urlService.ts

--- src/services/urlService.ts
+++ src/services/urlService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { UrlType } from '../types';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://urlshortener-140e.onrender.com/api';
 
 export const shortenUrl = async (originalUrl: string, customCode?: string): Promise<UrlType> => {
   try {
